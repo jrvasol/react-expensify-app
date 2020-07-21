37a2b66665c24a3889e060c653cee0358182c7a5
jrvasol <vasol@lapapa88.com>
2020-07-21 18:02:12 +0800

    Remove dummy expense data

--- src/App.js
+++ src/App.js
@@ -10,10 +10,6 @@ import configureStore from './store/configureStore'
 
 const store = configureStore();
 
-store.dispatch(addExpense({description: 'January Rent', amount: 54500, createdAt: 1000}))
-store.dispatch(addExpense({description: 'Coffee', amount: 500, createdAt: -1000}))
-store.dispatch(addExpense({description: 'Coffee', amount: 5400, createdAt: 2000}))
-
 function App() {
   return (
     <Provider store={store}>
